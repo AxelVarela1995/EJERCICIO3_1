72adfd2d4df3e4c45d392941a6f1775c49fd14c1
Axel Varela <axelvarela66@gmail.com>
2022-04-17 15:31:45 -0600

    Add project files.

---
+++ EJERCICIO3_1/EJERCICIO3_1/Models/EmpleDB.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using EJERCICIO3_1.Models;
+using SQLite;
+
+
+namespace EJERCICIO3_1.Models
+{
+    public class EmpleDB
+    {
+        readonly SQLiteAsyncConnection db;
+        //Constructor de clase vacio
+        public EmpleDB() { }
+
+        //Constructor con parametros
+        public EmpleDB(String pathbasedatos)
+        {
+            db = new SQLiteAsyncConnection(pathbasedatos);
+            // Creacion de tablas de base de datos
+            db.CreateTableAsync<Empleados>();
+        }
+      
+        //Retorna la tabla de empleados con una lista
+        public Task<List<Empleados>> listaempleados()
+        {
+            return db.Table<Empleados>().ToListAsync();
+        }
+        //Buscar un empleado especifico por el codigo
+        public Task<Empleados> ObtenerEmpleado(Int32 pcodigo)
+        {
+            return db.Table<Empleados>()
+                .Where(i => i.codigo == pcodigo)
+                .FirstOrDefaultAsync();
+        }
+
+        //Guardar o Actualizar Empleado
+
+        public Task<Int32> EmpleadoGuardar(Empleados emple)
+        {
+            if (emple.codigo != 0)
+            {
+                return db.UpdateAsync(emple);
+            }
+            else
+            {
+                return db.InsertAsync(emple);
+            }
+        }
+        //Eliminar Empleado
+        public Task<Int32> EmpleadoBorrar(Empleados emple)
+        {
+            return db.DeleteAsync(emple);
+        }
+
+        internal void ObtenerEmpleado()
+        {
+            throw new NotImplementedException();
+        }
+        public Task<List<Empleados>> GetEmpleados()
+        {
+            return db.Table<Empleados>().ToListAsync();
+        }
+        public Task<Empleados> GetEmpleadosByIdAsync(int pcodigo)
+        {
+            return db.Table<Empleados>().Where(a => a.codigo == pcodigo).FirstOrDefaultAsync();
+        }
+    }
+}
---
+++ EJERCICIO3_1/EJERCICIO3_1/Models/Empleados.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace EJERCICIO3_1.Models
+{
+   public class Empleados
+    {
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set; }
+
+        [MaxLength(100)]
+        public String nombres { get; set; }
+
+        [MaxLength(100)]
+        public String apellidos { get; set; }
+
+        public Int32 edad { get; set; }
+
+
+        [MaxLength(100)]
+        public String direccion { get; set; }
+
+        [MaxLength(100)]
+        public String puesto { get; set; }
+    }
+}
